a4037b284c69e2baaf76a96ba7549a42a80e750d
pazguille <guille87paz@gmail.com>
2022-11-07 11:21:06 -0300

    Update port to 8080

--- pie/server/index.js
+++ pie/server/index.js
@@ -5,7 +5,7 @@ const http = require('http');
 const app = require('./app.js');
 
 const host = '0.0.0.0';
-const port = process.env.PORT || 3031;
+const port = process.env.PORT || 8080;
 
 http.createServer(app).listen(port, host, () => {
   console.log(`App listening on port ${port}.`);
